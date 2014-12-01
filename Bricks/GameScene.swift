c34e893c68cd10f10dd4ff3445c5da21257566ac
Rico Zuniga <hello@ricozuniga.com>
2014-12-01 18:47:58 +0800

    Finished Drawing Tetrominoes.

--- Bricks/GameScene.swift
+++ Bricks/GameScene.swift
@@ -8,23 +8,52 @@
 
 import SpriteKit
 
+let colors: [SKColor] = [
+    SKColor.lightGrayColor(),
+    SKColor.cyanColor(),
+    SKColor.yellowColor(),
+    SKColor.magentaColor(),
+    SKColor.blueColor(),
+    SKColor.orangeColor(),
+    SKColor.greenColor(),
+    SKColor.redColor(),
+    SKColor.darkGrayColor()
+]
+
+let blockSize: CGFloat = 18.0
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let block = SKSpriteNode(color: SKColor.orangeColor(), size: CGSize(width: 50, height: 50))
-        block.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(block)
+        self.anchorPoint = CGPoint(x: 0, y: 1.0)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
+            drawTetrominoAtPoint(touch.locationInNode(self))
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func drawTetrominoAtPoint(location: CGPoint) {
+        let t = Tetromino()
+        for row in 0..<t.bitmap.count {
+            for col in 0..<t.bitmap[row].count {
+                if t.bitmap[row][col] > 0 {
+                    let block = t.bitmap[row][col]
+                    let square = SKSpriteNode(color: colors[block], size: CGSize(width: blockSize, height: blockSize))
+                    square.anchorPoint = CGPoint(x: 1.0, y: 0)
+                    square.position = CGPoint(x: col * Int(blockSize) + col, y: -row * Int(blockSize) + -row)
+                    square.position.x += location.x
+                    square.position.y += location.y
+                    self.addChild(square)
+                }
+            }
+        }
     }
 }
