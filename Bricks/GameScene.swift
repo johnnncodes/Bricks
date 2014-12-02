404b7f4e02141c919db68f8197e6a36a748d256a
Rico Zuniga <hello@ricozuniga.com>
2014-12-02 21:31:02 +0800

    Finished Collision Detection.

--- Bricks/GameScene.swift
+++ Bricks/GameScene.swift
@@ -20,25 +20,76 @@ let colors: [SKColor] = [
     SKColor.darkGrayColor()
 ]
 
+let gameBitmapDefault: [[Int]] = [
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
+]
+
 let blockSize: CGFloat = 18.0
 let defaultSpeed = NSTimeInterval(1200)
 
 class GameScene: SKScene {
     var dropTime = defaultSpeed
     var lastUpdate:NSDate?
+    
+    let gameBoard = SKSpriteNode()
+    var activeTetromino = Tetromino()
+    
+    var gameBitmapDynamic = gameBitmapDefault
+    var gameBitmapStatic = gameBitmapDefault
 
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         self.anchorPoint = CGPoint(x: 0, y: 1.0)
+
+        gameBoard.anchorPoint = CGPoint(x: 0, y: 1.0)
+        for col in 0..<gameBitmapDefault[0].count {
+            for row in 0..<gameBitmapDefault.count {
+                let bit = gameBitmapDefault[row][col]
+                let square = SKSpriteNode(color: colors[bit], size: CGSize(width: blockSize, height: blockSize))
+                square.anchorPoint = CGPoint(x: 0, y: 0)
+                square.position = CGPoint(x: col * Int(blockSize) + col, y: -row * Int(blockSize) + -row)
+                gameBoard.addChild(square)
+            }
+        }
+        
+        let gameBoardFrame = gameBoard.calculateAccumulatedFrame()
+        gameBoard.position = CGPoint(x: CGRectGetMidX(self.frame) - gameBoardFrame.width / 2, y: -125)
+        self.addChild(gameBoard)
+        
+        centerActiveTetromino()
+        refresh()
         lastUpdate = NSDate()
+    }
+    
+    func centerActiveTetromino() {
+        let cols = gameBitmapDefault[0].count
+        let brickWidth = activeTetromino.bitmap[0].count
+        activeTetromino.position = (cols / 2 -  brickWidth, 0)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            drawTetrominoAtPoint(touch.locationInNode(self))
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
@@ -46,33 +97,71 @@ class GameScene: SKScene {
         if lastUpdate != nil {
             let elapsed = lastUpdate!.timeIntervalSinceNow * -1000.0
             if elapsed > dropTime {
-                moveTetrominoesDown()
+                moveTetrominoDown()
             }
         }
     }
     
-    func drawTetrominoAtPoint(location: CGPoint) {
-        let t = Tetromino()
-        for row in 0..<t.bitmap.count {
-            for col in 0..<t.bitmap[row].count {
-                if t.bitmap[row][col] > 0 {
-                    let block = t.bitmap[row][col]
-                    let square = SKSpriteNode(color: colors[block], size: CGSize(width: blockSize, height: blockSize))
-                    square.anchorPoint = CGPoint(x: 1.0, y: 0)
-                    square.position = CGPoint(x: col * Int(blockSize) + col, y: -row * Int(blockSize) + -row)
-                    square.position.x += location.x
-                    square.position.y += location.y
-                    self.addChild(square)
+    func moveTetrominoDown() {
+        if landed() {
+            gameBitmapStatic.removeAll(keepCapacity: true)
+            gameBitmapStatic = gameBitmapDynamic
+            
+            activeTetromino = Tetromino()
+            centerActiveTetromino()
+        } else {
+            activeTetromino.moveTo(.Down)
+            
+        }
+        
+        lastUpdate = NSDate()
+        refresh()
+    }
+    
+    func landed() -> Bool {
+        let x = activeTetromino.position.x
+        let y = activeTetromino.position.y + 1
+        for row in 0..<activeTetromino.bitmap.count {
+            for col in 0..<activeTetromino.bitmap[row].count {
+                if activeTetromino.bitmap[row][col] > 0 && gameBitmapStatic[y + row][x + col + 1] > 0 {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    
+    func refresh() {
+        updateGameBitmap()
+        updateGameBoard()
+    }
+    
+    func updateGameBitmap() {
+        gameBitmapDynamic.removeAll(keepCapacity: true)
+        gameBitmapDynamic = gameBitmapStatic
+        
+        for row in 0..<activeTetromino.bitmap.count {
+            for col in 0..<activeTetromino.bitmap[row].count {
+                if activeTetromino.bitmap[row][col] > 0 {
+                    gameBitmapDynamic[activeTetromino.position.y + row][activeTetromino.position.x + col + 1] = activeTetromino.bitmap[row][col]
                 }
             }
         }
     }
     
-    func moveTetrominoesDown() {
-        let squares = self.children as [SKSpriteNode]
-        for square in squares {
-            square.position.y += CGFloat(-blockSize)
+    func updateGameBoard() {
+        let squares = gameBoard.children as [SKSpriteNode]
+        var currentSquare = 0
+        
+        for col in 0..<gameBitmapDynamic[0].count {
+            for row in 0..<gameBitmapDynamic.count {
+                let bit = gameBitmapDynamic[row][col]
+                let square = squares[currentSquare]
+                if square.color != colors[bit] {
+                    square.color = colors[bit]
+                }
+                ++currentSquare
+            }
         }
-        lastUpdate = NSDate()
     }
 }
